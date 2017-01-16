a13e3b4202c856df697d3621fc37f40befd04a56
SandeepPandey <s4n989@gmail.com>
2017-01-16 10:08:02 +0530

    Added association between events and products

--- models/events.js
+++ models/events.js
@@ -1,11 +1,24 @@
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('Event', {
+	var Event = sequelize.define('Event', {
 		id: {
 			type: DataTypes.INTEGER.UNSIGNED,
 			primaryKey: true,
 			autoIncrement: true
 		},
 		name: DataTypes.STRING,
-		product: DataTypes.STRING
+		product_id: {
+			type: DataTypes.INTEGER.UNSIGNED,
+			allowNull: true
+		}
+	}, {
+		classMethods: {
+			associate: function(models) {
+				Event.belongsTo(models.Product, {
+					foreignKey: 'product_id'
+				});
+			}
+		}
 	});
+
+	return Event;
 };
