bc57108ed0ac450dc1e1d67c2b25a301ef421219
@chowmean [Gaurav Yadav] <gaurav.dev.iiitm@gmail.com>
2017-01-31 18:08:24 +0530

    added all context type

--- models/eventsLists.js
+++ models/eventsLists.js
@@ -9,7 +9,17 @@ module.exports = function(sequelize, DataTypes) {
 		action_name: DataTypes.STRING,
 		object_name: DataTypes.STRING,
 		platform_name: DataTypes.STRING,
-		context: DataTypes.STRING
+		object_context: DataTypes.STRING,
+		actor_context: DataTypes.STRING,
+		action_context: DataTypes.STRING,
+		device_context: DataTypes.STRING,
+		web_context: DataTypes.STRING,
+		app_context: DataTypes.STRING,
+		geolocation_context: DataTypes.STRING,
+		page_context: DataTypes.STRING,
+		screen_context: DataTypes.STRING,
+		attribution_context: DataTypes.STRING,
+		product_context: DataTypes.STRING
 	});
 	return EventList;
 };
